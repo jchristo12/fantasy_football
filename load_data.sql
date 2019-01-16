2c225144f9c2a73c6c55f94e2fcb80d38bdd4004
Joe <christoffjoe@gmail.com>
2019-01-15 23:03:14 -0600

    Change single quote to double quote

--- load_data.sql
+++ load_data.sql
@@ -1,6 +1,6 @@
-load data local infile 'C:/Users/Joe/Downloads/armchair_analysis/BLOCK.csv'
-into table nfl_stats.BLOCK
-fields terminated by ','
-lines terminated by '\r\n'
+load data local infile "C:/Users/Joe/Downloads/armchair_analysis/DEFENSE.csv"
+into table nfl_stats.DEFENSE
+fields terminated by ","
+lines terminated by "\n"
 ignore 1 lines
 ;
