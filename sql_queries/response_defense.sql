d60bc40ce5c12615b59491e920871fc53dbc4bdb
Joe <christoffjoe@gmail.com>
2019-01-17 15:47:21 -0600

    Remove calculated field

--- sql_queries/response_defense.sql
+++ sql_queries/response_defense.sql
@@ -1,10 +1,12 @@
 select GAME.seas, GAME.wk, DEFENSE.team,
-	sum(DEFENSE.sck), sum(DEFENSE.saf), sum(DEFENSE.blk), sum(DEFENSE.ints), sum(DEFENSE.frcv),
-	sum(DEFENSE.tdd), sum(DEFENSE.tdret),
+	sum(DEFENSE.sck) as sck, sum(DEFENSE.saf) as saf, sum(DEFENSE.blk) as blk, 
+    sum(DEFENSE.ints) as ints, sum(DEFENSE.frcv) as frcv, sum(DEFENSE.tdd) as tdd,
+    sum(DEFENSE.tdret) as tdret,
     if(DEFENSE.team = GAME.v, avg(GAME.ptsh), avg(GAME.ptsv)) as pts_allow
 from DEFENSE
 left join GAME
 	on DEFENSE.gid = GAME.gid
 where GAME.wk <= 17
 group by GAME.seas, GAME.wk, DEFENSE.team, GAME.v, GAME.h
+-- limit 10
 ;
