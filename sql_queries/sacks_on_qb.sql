431fa0ecfb2aba29c0ca071ffef91d07d72aea3c
Joe <christoffjoe@gmail.com>
2019-01-17 15:48:08 -0600

    Initial commit

---
+++ sql_queries/sacks_on_qb.sql
@@ -0,0 +1,9 @@
+select GAME.seas, GAME.wk, SACK.qb, count(SACK.pid) as tot_sack
+from SACK
+left join PLAY
+	on SACK.pid = PLAY.pid
+left join GAME
+	on PLAY.gid = GAME.gid
+where GAME.wk <= 17
+group by GAME.seas, GAME.wk, SACK.qb
+;
