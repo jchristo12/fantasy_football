901b1d88d65a13247aca2ccfda5c3193d5111ca6
Joe <christoffjoe@gmail.com>
2019-01-17 15:48:29 -0600

    Remove calculated field

--- sql_queries/response_offense.sql
+++ sql_queries/response_offense.sql
@@ -1,16 +1,6 @@
-select GAME.seas, GAME.wk, PLAYER.pos1, OFFENSE.py, OFFENSE.ints, OFFENSE.tdp, OFFENSE.ry,
-	OFFENSE.tdr, OFFENSE.recy, OFFENSE.tdrec, OFFENSE.rety, OFFENSE.tdret, OFFENSE.fuml,OFFENSE.conv,
-    round(OFFENSE.py / 25 +
-			OFFENSE.ints * -2 +
-            OFFENSE.tdp * 4 +
-            OFFENSE.ry / 10 +
-            OFFENSE.tdr * 6 +
-            OFFENSE.recy / 10 +
-            OFFENSE.tdrec * 6 +
-			OFFENSE.rety / 35 +
-			OFFENSE.tdret * 6 +
-			OFFENSE.fuml * -2 +
-			OFFENSE.conv * 2, 2) as fpts_off
+select GAME.seas, GAME.wk, PLAYER.player, PLAYER.pos1, OFFENSE.py, OFFENSE.ints, OFFENSE.tdp,
+	OFFENSE.ry, OFFENSE.tdr, OFFENSE.recy, OFFENSE.tdrec, OFFENSE.rety, OFFENSE.tdret,
+    OFFENSE.fuml, OFFENSE.conv
 from OFFENSE
 left join GAME
 	on OFFENSE.gid = GAME.gid
@@ -24,4 +14,5 @@ where GAME.wk <= 17
 	PLAYER.pos1 = "TE" or 
 	PLAYER.pos1 = "K"
 	)
+-- limit 10
 ;
