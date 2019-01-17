d954f2511b6d1b9168fb03d55cb9b706d5079593
Joe <christoffjoe@gmail.com>
2019-01-17 16:40:15 -0600

    change the way to change column type

--- analysis/response_variable.py
+++ analysis/response_variable.py
@@ -13,27 +13,34 @@ r_sacks = pd.read_csv('https://raw.githubusercontent.com/jchristo12/fantasy_foot
 
 #convert data types in columns
 #offense
-r_offense.seas = r_offense.seas.astype('category')
-r_offense.wk = r_offense.wk.astype('category')
-r_offense.player = r_offense.player.astype('str')
-r_offense.pos1 = r_offense.pos1.astype('category')
+r_offense = r_offense.astype({'pk': str,
+                          'seas': 'category',
+                          'wk': 'category',
+                          'player': str,
+                          'pos1': 'category'})
 
 #defense
-r_defense.seas = r_defense.seas.astype('category')
-r_defense.wk = r_defense.wk.astype('category')
-r_defense.team = r_defense.team.astype('category')
+r_defense = r_defense.astype({'pk': str,
+                          'seas': 'category',
+                          'wk': 'category',
+                          'team': 'category'})
 
 #kicker
-r_kicker.seas = r_kicker.seas.astype('category')
-r_kicker.wk = r_kicker.wk.astype('category')
-r_kicker.fkicker = r_kicker.fkicker.astype('category')
-r_kicker.good = r_kicker.good.astype('category')
+r_kicker = r_kicker.astype({'pk': str,
+                          'seas': 'category',
+                          'wk': 'category',
+                          'fkicker': str,
+                          'good': 'category'})
 #change column name of fkicker
 r_kicker.rename(columns={'fkicker': 'player'}, inplace=True)
 
 #sacks
-r_sacks.seas = r_sacks.seas.astype('category')
-r_sacks.wk = r_sacks.wk.astype('category')
-r_sacks.qb = r_sacks.qb.astype('str')
+r_sacks = r_sacks.astype({'pk': str,
+                          'seas': 'category',
+                          'wk': 'category',
+                          'qb': str})
 #rename qb to player
 r_sacks.rename(columns={'qb': 'player'}, inplace=True)
+
+#combine offense and sacks (for QBs)
+r_offense_test = r_offense.join(r_sacks, on='pk', how='left')
