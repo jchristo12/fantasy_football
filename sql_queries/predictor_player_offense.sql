fdb83a78872402141149a07f9adf662d566b7031
Joe <christoffjoe@gmail.com>
2019-02-24 17:05:21 -0600

    Initial commit

---
+++ sql_queries/predictor_player_offense.sql
@@ -0,0 +1,21 @@
+select concat(OFFENSE.gid, "&", OFFENSE.player) as pk,
+	-- identifying features
+    OFFENSE.gid, GAME.seas, GAME.wk, OFFENSE.player, OFFENSE.team, PLAYER.pos1,
+    -- passing stats
+    OFFENSE.pa, OFFENSE.pc, OFFENSE.py, OFFENSE.ints, OFFENSE.tdp,
+    -- rushing stats
+    OFFENSE.ra, OFFENSE.sra, OFFENSE.ry, OFFENSE.tdr, OFFENSE.fuml,
+    -- receiving stats
+    OFFENSE.trg, OFFENSE.rec, OFFENSE.recy, OFFENSE.tdrec,
+    -- other stats
+    OFFENSE.seas, PLAYER.height, PLAYER.weight, PLAYER.dob, PLAYER.dv,
+    -- rookie combine stats
+    PLAYER.forty, PLAYER.bench, PLAYER.vertical, PLAYER.broad, PLAYER.shuttle, PLAYER.cone,
+		PLAYER.arm, PLAYER.hand
+from OFFENSE
+left join GAME
+	on OFFENSE.gid = GAME.gid
+left join PLAYER
+	on OFFENSE.player = PLAYER.player
+-- limit 10
+;
