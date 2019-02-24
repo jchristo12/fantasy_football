c5e12e0c030422cd7a1429266ad5b3b37fd5e30e
Joe <christoffjoe@gmail.com>
2019-02-24 16:04:34 -0600

    Delete file and convert to jupyter notebook

--- analysis/data_analysis.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-import numpy as np
-
-#load the data
-offense = pd.read_csv('C:/Users/Joe/Projects/fantasy_football/analysis/offense_final.csv', index_col='pk')
-defense = pd.read_csv('C:/Users/Joe/Projects/fantasy_football/analysis/defense_final.csv', index_col='pk')
-kicker = pd.read_csv('C:/Users/Joe/Projects/fantasy_football/analysis/kicker_final.csv', index_col='pk')
