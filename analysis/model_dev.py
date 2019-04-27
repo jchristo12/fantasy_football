fb477ae4fedbb00b1f5e8372a301e3d5c972c86c
Joe <christoffjoe@gmail.com>
2019-04-26 22:58:14 -0500

    Initial data setup

--- analysis/model_dev.py
+++ analysis/model_dev.py
@@ -7,3 +7,49 @@ import matplotlib.pyplot as plt
 import random
 import time
 
+
+# =============================================================================
+# Functions
+# =============================================================================
+def divide_by_pos_wk(data, pos, wk):
+    """
+    Take a dataframe, position, and week and return the subset of the data in a new dataframe.
+    """
+    df = data[(data['pos1']==pos) & (data['wk']==wk)]
+    return df
+    
+
+
+# =============================================================================
+# Data Setup
+# =============================================================================
+#import the data
+df = pd.read_csv('https://github.com/jchristo12/fantasy_football/blob/master/data/full_data.csv?raw=true')
+
+#remove rows that have NaN for the shifted variables
+df_clean = df[~df.loc[:,'seas_pa':'seas_tdret'].isna().all(axis=1)]
+
+#store positions we are concerned about; will use these to filter out 
+pos_of_interest = ['QB', 'RB', 'WR', 'TE']
+#filter out positions we don't care about
+df_clean2 = df_clean[df_clean['pos1'].isin(pos_of_interest)]
+
+#set the column types
+col_dtypes = {'category': ['seas', 'wk', 'pos1', 'team', 'udog', 'v', 'h', 'day', 'stad', 'wdir',
+                          'surf', 'gen_cond', 'gen_dv', 'def_team']}
+#flip the key and values around so they will work in the argument for 'astype()'
+col_dtypes_alt = {old: new for new, old_all in col_dtypes.items() for old in old_all}
+#make the change to column type
+df_clean2 = df_clean2.astype(col_dtypes_alt)
+
+#store dataframe of non-rookies
+df_vet = df_clean2.loc[df_clean2['exp']!=1, :]
+#store dataframe of rookie data
+df_rook = df_clean2.loc[df_clean2['exp']==1, :]
+
+#segment out for WR and week 10
+df_wr10 = divide_by_pos_wk(df_vet, 'WR', 10)
+
+# =============================================================================
+# Data Analysis
+# =============================================================================
