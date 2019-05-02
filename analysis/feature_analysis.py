7614a20db6978fee3031e6b16f666317c416ad02
Joe Christoff <40745710+jchristo12@users.noreply.github.com>
2019-05-02 10:32:46 -0500

    fix typo of 'UDF' to 'udf'

--- analysis/feature_analysis.py
+++ analysis/feature_analysis.py
@@ -4,7 +4,7 @@ def main():
         import pandas as pd
         import numpy as np
         import math
-        from python_pkg import python_udf as UDF
+        from python_pkg import python_udf as udf
     except:
         print('Necessary packages not installed. Please install:')
         print('numpy, pandas, math, and python_pkg (UDF)')
